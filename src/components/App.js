03b9743a3ae6062570c1ebca0932441f12889180
Mwambui123 <maurinawambuia@gmail.com>
2023-11-25 18:01:20 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ function App() {
   return <div>
       <NavBar />
       <Switch>
-      <Route exact path="/movies"> <Movies/></Route>
-      <Route exact path="/directors"> <Directors/></Route>
-      <Route exact path="/actors" ><Actors/></Route>
-      <Route exact path="/"><Home/></Route> 
+      <Route exact path="/movies"> <Movies /></Route>
+      <Route exact path="/directors"> <Directors /></Route>
+      <Route exact path="/actors" ><Actors /></Route>
+      <Route exact path="/"><Home /></Route> 
 
       </Switch>
       </div>;
